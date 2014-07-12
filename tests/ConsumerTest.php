490c9166cb3283cffde95125eba28aede8433069
aculp <adamculp@uws.net>
2014-07-11 23:37:41 -0400

    Added tests for Params set/get

--- tests/ConsumerTest.php
+++ tests/ConsumerTest.php
@@ -7,15 +7,30 @@ use ApiConsumer\Consumer;
 class ConsumerTest extends \PHPUnit_Framework_TestCase
 {
     
-    public function testSetUrl()
+    public function testSetAndGetUrl()
     {
         $consumer = new Consumer();
-        
+
         $consumer->setUrl('http://uws.net');
-        
+
         $result = $consumer->getUrl();
-        
+
         $this->assertEquals('http://uws.net', $result);
     }
     
+    public function testSetAndGetParams()
+    {
+        $consumer = new Consumer();
+        
+        $testParams = array('k' => 'ultra+marathon','v' => 'json','l' => 'Florida');
+        
+        $consumer->setParams($testParams);
+        
+        $result = $consumer->getParams();
+        
+        foreach ($testParams as $key => $value) {
+            $this->assertArrayHasKey($key, $result);
+            $this->assertEquals($value, $result[$key]);
+        }
+    }
 }
